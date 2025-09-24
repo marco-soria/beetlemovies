3cd27352e02794b8be6f84e3579cc78b8fd5aa6e
marco-soria <marco.soriaa7@gmail.com>
2025-09-24 14:51:19 -0500

    reorganized minimal api structure

---
+++ BeetleMovies.API/EndpointHandlers/DirectorsHandlers.cs
@@ -0,0 +1,25 @@
+using AutoMapper;
+using BeetleMovies.API.DBContext;
+using BeetleMovies.API.DTOs;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeetleMovies.API.EndpointHandlers;
+
+
+public static class DirectorsHandlers
+{
+  public static async Task<Results<NotFound, Ok<IEnumerable<DirectorDTO>>>> GetDirectorsAsync (
+  BeetleMoviesContext context,
+  IMapper mapper,
+  int moviesId)
+  {
+    var movieResult = await context.Movies.FirstOrDefaultAsync(x => x.Id == moviesId);
+      if (movieResult == null)
+        return TypedResults.NotFound();
+
+    return TypedResults.Ok(mapper.Map<IEnumerable<DirectorDTO>>((await context.Movies
+                        .Include(movie => movie.Directors)
+                        .FirstOrDefaultAsync(movie => movie.Id == moviesId))?.Directors));
+  }
+}
